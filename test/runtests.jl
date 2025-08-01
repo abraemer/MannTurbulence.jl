314963a245bb6d903058faebbe1e9a8309116875
Adrian Braemer <adrian.braemer@tngtech.com>
2025-08-01 17:13:08 +0200

    reenable Aqua and JET

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,12 @@ using JET
 include("test_helpers.jl")
 
 @testset "MannTurbulence.jl" begin
-    # @testset "Code quality (Aqua.jl)" begin
-    #     Aqua.test_all(MannTurbulence)
-    # end
-    # @testset "Code linting (JET.jl)" begin
-    #     JET.test_package(MannTurbulence; target_defined_modules = true)
-    # end
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(MannTurbulence)
+    end
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(MannTurbulence; target_defined_modules = true)
+    end
 
     # Include tensor tests
     @testset "tensors.jl" include("test_tensors.jl")
